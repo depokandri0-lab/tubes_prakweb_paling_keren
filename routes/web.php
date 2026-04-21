9b873fae4966a2a546c0cf8b07d4837fb99792be
mhmdryhns <smkiu.009@gmail.com>
2026-04-21 08:53:04 +0700

    pertambahan

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('welcome2', function () {
     return view('welcome.welcome2');
 
+});
 Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
