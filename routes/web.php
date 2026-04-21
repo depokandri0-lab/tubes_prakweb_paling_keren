6d77c4cf850393626c822d296ee50ebd33300089
depokandri0 <depokandri0@gmail.com>
2026-04-21 08:47:32 +0700

    perubahan

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('welcome2', function () {
     return view('welcome.welcome2');
 
+});
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::get('selamat jalan', function () {
+    return view('selamat jalan.selamatjalan2');
+});
+
